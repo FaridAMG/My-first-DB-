f36a96582cf8c2638f8dc0a50afc1d7930f1801f
farid <farid@FaridAMG>
2017-08-30 01:38:01 -0600

    Commit No.1  30/08/17... se creo las interfaces

---
+++ src/application/MyFirstDB.java
@@ -0,0 +1,26 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MyFirstDB extends Application{
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
+	@Override
+	public void start(Stage PrimaryStage) throws Exception {
+		Parent gui = FXMLLoader.load(getClass().getResource("/myFirstDB/guis/fxml/pagPrincipal.fxml"));
+		Scene scene = new Scene(gui);
+		
+		PrimaryStage.setScene(scene);
+		PrimaryStage.setTitle("MyFirstDB!!");
+		PrimaryStage.show();
+	}
+
+}
