d01564c4f23c426b9b865846871fe400bb95f110
farid <farid@FaridAMG>
2017-09-20 00:06:41 -0600

    Este commit es bastante importante...

--- src/application/MyFirstDB.java
+++ src/application/MyFirstDB.java
@@ -1,21 +1,25 @@
 package application;
 
+
+
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class MyFirstDB extends Application{
 
 	public static void main(String[] args) {
 		launch(args);
-
 	}
 
 	@Override
 	public void start(Stage PrimaryStage) throws Exception {
-		Parent gui = FXMLLoader.load(getClass().getResource("/myFirstDB/guis/fxml/pagPrincipal.fxml"));
+	    Parent gui = FXMLLoader.load(getClass().getResource("/myFirstDB/guis/fxml/pagPrincipal.fxml"));
 		Scene scene = new Scene(gui);
 		
 		PrimaryStage.setScene(scene);
